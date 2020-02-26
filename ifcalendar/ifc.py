874d18c62c15bbb4c93b3bd03523c9eebda5f417
sandeepa <alimisandeep97@gmail.com>
2020-02-26 11:35:12 +0530

    added year day and leap day

--- ifcalendar/ifc.py
+++ ifcalendar/ifc.py
@@ -4,10 +4,13 @@ International Fixed Calander or Cotsworth Calender is a 13 month fixed calender.
 
 # https://github.com/python/cpython/blob/master/Lib/datetime.py
 
-from datetime import datetime as _datetime
+from datetime import datetime
+
 
 MAX_MONTH = 13
 MAX_DAYS = 28
+LEAP_DAY = 'Sun'
+YEAR_DAY = 'Sun'
 
 MONTHS = '''
 January
@@ -37,14 +40,12 @@ Sat
 
 
 def ifc_now():
-    tt = _datetime.now()
-    return IFCDate(tt.timetuple().tm_yday, tt.year)
-
+    return datetime.now().timetuple().tm_yday
 
 def leap_year(year):
     if (year % 4) == 0:
-        if (year % 100 == 0):
-            if (year % 400 == 0):
+        if(year % 100 ==0 ):
+            if (year % 400 ==0):
                 return True
             else:
                 return False
@@ -53,16 +54,34 @@ def leap_year(year):
     else:
         return False
 
+def get_week_ct(day,is_leap):
 
-def get_week_ct(day):
+    #Neither Year Day nor Leap Day are considered to be part of any week as per IFC
+
+    if is_leap:
+        if day==169:
+            return 0
+        if day==366:
+            return 0
+        if day>169:
+            day=day-1
+
     return day // 7
 
 
-def get_day_of_week(day):
-    return day % 7 - 1
+def get_week_day(day,is_leap):
 
+    if is_leap:
+        if day==169:
+            return LEAP_DAY
+        if day==366:
+            return YEAR_DAY
+        if day>169:
+            day=day-1
+    else:
+        if day==365:
+            return YEAR_DAY
 
-def get_week_day(day):
     return WEEKDAYS[day % 7 - 1]
 
 
@@ -70,60 +89,81 @@ def get_month_name(mon_ct):
     return MONTHS[mon_ct - 1][:3].upper()
 
 
-def get_month_and_day(day_ct):
+def get_month(day_ct,is_leap):
     if not (0 < day_ct < 367):
         raise Exception("DayCount is not between 0 and 366")
+
+    if is_leap:
+        if day_ct==169:
+            return 6
+        if day_ct>169:
+            day_ct=day_ct
 
     if day_ct == 365 or day_ct == 366:
-        return 13, 29
+        return 13
 
     mon, day = divmod(day_ct, MAX_DAYS)
-    if day == 0 & mon != 13:
-        return mon, day
+    if day == 0 & mon!=13:
+        return mon
+   
+    return mon+1
 
-    return mon + 1, day
-
-
-def get_day(day_ct):
+def get_day(day_ct,is_leap):
     if not (0 < day_ct < 367):
         raise Exception("DayCount is not between 0 and 366")
 
-    rem = (day_ct % MAX_DAYS)
-    if rem == 0:
+    if is_leap:
+        if day_ct==169:
+            return 29
+        if day_ct==366:
+            return 29
+        if day_ct==365:
+            return 28
+        if day_ct>169:
+            day_ct=day_ct-1
+    else:
+        if day_ct==366:
+            return "Not a leap year to exist 366 days"
+        if day_ct==365:
+            return 29
+
+    rem=(day_ct % MAX_DAYS)
+    if rem==0:
         return MAX_DAYS
     else:
         return rem
 
 
 def to_ifc(obj_datetime):
-    if not obj_datetime.__class__ == _datetime:
+    if not obj_datetime.__class__ == datetime:
         raise Exception('Expected `datetime` class object received `{}`'.format(obj_datetime.__class__))
     time_tuple = obj_datetime.timetuple()
     return CotsDate(time_tuple.tm_yday, time_tuple.tm_year)
 
 
 class CotsDate:
-    def __init__(self, dayofyear, year):
-        self._dayofyear = dayofyear
+    def __init__(self, year_day_count, year):
+        self._year_day_count = year_day_count
         self._year = year
+        self.is_leap = leap_year(year)
 
     def get_month_id(self):
-        return get_month_and_day(self._dayofyear)[0]
+        return get_month(self._year_day_count,self.is_leap)
 
     def get_month_name(self):
         return get_month_name(self.month)
 
     def get_week_id(self):
-        return get_week_ct(self._dayofyear)
+        return get_week_ct(self._year_day_count,self.is_leap)
 
     def get_week_day(self):
-        return get_week_day(self._dayofyear)
+        return get_week_day(self._year_day_count,self.is_leap)
 
     def get_year(self):
         return self._year
 
     def get_day(self):
-        return get_day(self._year_day_count)
+        return get_day(self._year_day_count,self.is_leap)
 
     def __repr__(self):
         return '{} {} {} {}'.format(self.week_day, self.day, self.month_name, self.year)
@@ -138,8 +178,9 @@ class CotsDate:
 
 def print_year():
     for i in range(1, 367):
-        t = CotsDate(i, 2020)
-        print(t.day, t.week, t.week_day, t.month, t.month_name, t.year)
-
+        t = CotsDate(i, 2019)
+        print(i,t.day, t.week, t.week_day, t.month, t.month_name, t.year)
+      
 
 IFCDate = CotsDate
+print_year()
